f21f3e0923483f3a7890bcca5357261b5f9cc835
Rafi Ser <89293629+RAFI-SER@users.noreply.github.com>
2022-06-22 13:33:47 +0530

    fix

--- plugins/sticker-wm.js
+++ plugins/sticker-wm.js
@@ -5,8 +5,8 @@ let handler = async (m, { conn, text }) => {
   if (!m.quoted) throw 'Quoted the sticker!'
   let stiker = false
   try {
-    let [packname, ...author] = text.split('|')
-    author = (author || []).join('|')
+    let [packname, ...author] = text.split(',')
+    author = (author || []).join(',')
     let mime = m.quoted.mimetype || ''
     if (!/webp/.test(mime)) throw 'Reply sticker!'
     let img = await m.quoted.download()
