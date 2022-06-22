0153604e57aa1dc2c56644c6bd4b4be038b4eeca
Rafi Ser <89293629+RAFI-SER@users.noreply.github.com>
2022-06-22 13:21:46 +0530

    ғɪx

--- plugins/youtube-yta.js
+++ plugins/youtube-yta.js
@@ -1,7 +1,7 @@
 let limit = 80
 import { youtubedl, youtubedlv2 } from '@bochilteam/scraper';
 let handler = async (m, { conn, args, isPrems, isOwner }) => {
-    if (!args || !args[0]) throw 'Wheres the url?'
+    if (!args || !args[0]) throw '𝑾𝑯𝑬𝑹𝑬 𝑰𝑺 𝑻𝑯𝑬 𝑳𝑰𝑵𝑲 𝑴𝑨𝑵?'
 let { thumbnail, audio, title } = await youtubedl(args[0])
     .catch(async () => await youtubedlv2(args[0]))
 let link = await audio['128kbps'].download()
@@ -9,20 +9,20 @@ const isY = /y(es)/gi.test(args[1])
 const limitedSize = (isPrems || isOwner ? 99 : 70) * 1024
 let isLimit = limitedSize < audio['128kbps'].fileSize
 if (!isY) await conn.sendFile(m.chat, thumbnail, 'thumbnail.jpg', `
-*🔮𝗧𝗜𝗧𝗟𝗘:* ${title}
-*🔮𝗙𝗜𝗟𝗘𝗦𝗜𝗭𝗘 :* ${audio['128kbps'].fileSizeH}
+*🔮𝑻𝑰𝑻𝑳𝑬:* ${title}
+*🔮𝑭𝑰𝑳𝑬𝑺𝑰𝒁𝑬 :* ${audio['128kbps'].fileSizeH}
 *${isLimit ? 'Use ' : ''}Link:* ${link}
 `.trim(), m)
 if (!isLimit) await conn.sendFile(m.chat, link, title + '.mp3', `
-*🔮𝗧𝗜𝗧𝗟𝗘:* ${title}
-*🔮𝗙𝗜𝗟𝗘𝗦𝗜𝗭𝗘:* ${audio['128kbps'].fileSizeH}
+*🔮𝑻𝑰𝑻𝑳𝑬:* ${title}
+*🔮𝑭𝑰𝑳𝑬𝑺𝑰𝒁𝑬:* ${audio['128kbps'].fileSizeH}
 `.trim(), m, null, {
 asDocument: 1
 })
 }
-handler.help = ['mp3', 'a'].map(v => 'yt' + v + ` <ur`)
+handler.help = ['song', 'yta'].map(v => 'yt' + v + ` <ur`)
 handler.tags = ['downloader']
-handler.command = /^yt(a|mp3)$/i
+handler.command = /^yt(a|song)$/i
 handler.limit = 1
 
 handler.exp = 0
